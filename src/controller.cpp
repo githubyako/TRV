cab1b93c13ec32015b4e81883c1f4267ec32b63d
tvoillemin <theo.voillemin@laposte.net>
2016-04-07 15:45:04 +0200

    Added files via upload

--- src/controller.cpp
+++ src/controller.cpp
@@ -52,7 +52,51 @@ void Controller::supprimer_agent(int id)
 
 void Controller::initiateMap(std::string contentFileName)
 {
-  //Parse fichier .txt du contenu de la carte
+  int x;
+  int y;
+  std::string type_terr;
+  std::string contrainte ="";
+  std::string::size_type sz;
+  int obst=0;
+  int i;
+  std::vector<std::string> splitter;
+  std::ifstream fichier (F.c_str(), std::ios::in);
+  if(fichier)
+  {
+    std::string contenu;
+    std::getline(fichier, contenu);
+    splitter = split(contenu, ' ');
+    x = atoi(splitter[0].c_str());
+    y = atoi(splitter[1].c_str());
+    Map M(x,y);
+    while (std::getline(fichier, contenu))
+    {
+      splitter.clear();
+      splitter = split(contenu, ' ');  
+      x = atoi(splitter[0].c_str());
+      y = atoi(splitter[1].c_str());
+      type_terr = splitter[2];
+      switch (splitter.size())
+      {
+	case 4:
+	  contrainte = splitter[3];
+	  M.getCase(x,y).setContrainte(contrainte);
+	  break;
+	case 5:
+	  contrainte = splitter[3];
+	  obst = atoi(splitter[4].c_str());
+	  M.getCase(x,y).setContrainte(contrainte);
+	  M.getCase(x,y).setObstacle(obst);
+	  break;
+      }
+      M.getCase(x,y).setTerrain(M.getTerrain(type_terr));
+    }
+    fichier.close();
+  }
+   else
+   {
+     std::cout << "Impossible d'ouvrir le fichier !" << std::endl;
+   }
 }
 
 void Controller::initiateRules(std::string xmlFileName)
@@ -68,6 +112,19 @@ void Controller::initiateRules(std::string xmlFileName)
   NodeSet unites = rootNode->find("/regle/unites");
   Node* firstNodeInResult = result.at(0);
   
+}
+
+std::vector< std::string > Controller::split(std::string str, char delimiter)
+{
+  std::vector<std::string> internal;
+  std::stringstream ss(str);
+  std::string tok;
+  
+  while(std::getline(ss, tok, delimiter))
+  {
+    internal.push_back(tok);
+  }
+  return internal;
 }
 
 

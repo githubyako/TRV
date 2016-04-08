f287d3854019c0e66b9996ff50eb23c37ff83fdb
tvoillemin <theo.voillemin@laposte.net>
2016-04-08 11:07:27 +0200

    Update terrain.h

--- src/terrain.h
+++ src/terrain.h
@@ -6,7 +6,7 @@
 class Terrain{
 private:
   std::string m_type;
-  std::vector<std::pair<std::string, float> > m_contraintes_defaut;
+  Contrainte* m_contraintes_defaut;
   bool m_obstacle;
 public:
   Terrain(std::string & _type, Contrainte* _contraintes_defaut, bool _obstacle);

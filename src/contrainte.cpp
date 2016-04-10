043b8bd42aebbdd9475c6c3d5dadde40b94eef5d
tvoillemin <theo.voillemin@laposte.net>
2016-04-10 11:20:54 +0200

    Update contrainte.cpp

--- src/contrainte.cpp
+++ src/contrainte.cpp
@@ -1,25 +1,29 @@
 #include "contrainte.h"
 
+// *************
+// Constructeurs
+// *************
+
 Contrainte::Contrainte(const std::string& _nom)
 :m_nom(_nom)
-{
-
-}
+{}
 
 Contrainte::Contrainte(const Contrainte& _contrainte)
-{
-
-}
+{}
 
 Contrainte::Contrainte()
-{
+{}
 
-}
+// ***********
+// Destructeur
+// ***********
 
 Contrainte::~Contrainte()
-{
+{}
 
-}
+// *******
+// Getteur
+// *******
 
 std::string const & Contrainte::getNom() const
 {

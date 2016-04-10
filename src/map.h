e8902de9ebb97a650b319bc933508ff5329acd3f
tvoillemin <theo.voillemin@laposte.net>
2016-04-10 17:32:34 +0200

    Update map.h

--- src/map.h
+++ src/map.h
@@ -9,34 +9,56 @@
 class Map{
 private:
   static Map* m_map;
-  int m_w,m_h;
-  std::map<int, Case*> m_sommets;
-  std::vector<Terrain*> m_terrains;
-  std::vector<Agent*> m_agents;
-  std::vector<Unite*> m_unites;
-  std::vector<Contrainte*> m_contraintes;
+  int m_w,m_h; // Nombre de case en x (m_w) et nombre de case en y (m_h) de la map
+  std::map<int, Case*> m_sommets; // Map associant à un indice int, une case Case*
+  std::vector<Terrain*> m_terrains; // Vector contenant tout les terrains décrient dans regle.xml
+  std::vector<Agent*> m_agents; // Vector contenant tous les agents du jeu. Si on rajoute un agent, on rajoute un élément un vecteur, si on le supprime, on mets cette élément à NULL
+  std::vector<Unite*> m_unites; // Vector contenant toutes les unités décrient dans regle.xml
+  std::vector<Contrainte*> m_contraintes; // Vector contenant toutes les contraintes décrient dans regle.xml
  
 public:
+  // Constructeurs :
   Map();
   Map(Map const & _map);
   Map( int _w,  int _h);
+  
   static Map* create(int _w, int _h);
+  
+  // Fonction d'ajout de Terrain avec ou sans obstacle) à la map
   void addTerrain(std::string const & _type, std::vector<std::pair<std::string const &, float> > & _contraintes_defaut);
   void addTerrain(std::string const & _type, std::vector<std::pair<std::string const &, float> > & _contraintes_defaut, bool _obstacle);
+  
+  // Fonction d'ajout d'Unité à la map 
   void addUnite(const std::string& _type, std::vector< std::pair< std::string const&, float > >& _vitesse_d, 
 		std::vector< std::pair< std::string const&, float > >& _consoContraintes);
+		
+  // Fonction d'ajout d'un agent à la map
   void addAgent(int _iden, int _x, int _y, std::string const &_unite);
+  
+  // Fonction d'ajout d'une Contrainte à la map
   void addContrainte(std::string const& _contrainte);
+  
+  // Fonction pour la déplacement d'un agent d'identifiant id et à la "position" id du vector m_agents vers la case de coordonnées x,y
   void move_agent(int id, int x, int y);
+  
+  // Fonction pour la suppresion d'un agent d'identifiant id et à la "position" id du vector m_agents
   void suppr_agent(int id);
+  
+  // Getteurs :
   int get_m_w() const;
   int get_m_h() const;
   Case* get_Case(int _x, int _y) const;
   Agent* get_Agent(int id) const;
   Terrain* get_Terrain(std::string const & _terrName) const;
   Contrainte * get_Contrainte(std::string const & _contrName) const;
+  
+  // Fonction pour changer le type de la case de coordonnée x,y en terrain de type _terrName
   void set_Terrain(int _x, int _y, std::string const & _terrName);
+  
+  // Fonction changeant le booléen obst de la case à la coordonnée x,y
   void set_Obstacle(int _x, int _y, int obst);
+  
+  // Destructeur
   ~Map();
   
 };

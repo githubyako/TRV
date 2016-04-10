c1a7cc900d2754e8101d997707a7961ad16c7e63
tvoillemin <theo.voillemin@laposte.net>
2016-04-10 11:04:39 +0200

    Update agent.h

--- src/agent.h
+++ src/agent.h
@@ -4,21 +4,28 @@
 #include "unite.h"
 #include "case.h"
 
+// Classe représentant les agents se déplaçant sur la Map
+
 class Agent{
   
 private:
-  int m_id;
-  float m_multiplicateur_v;
-  Unite *m_unite;
-  Case *m_case;
+  int m_id; // Identificateur d'un agent
+  float m_multiplicateur_v; // Multiplicateur de vitesse de l'agent (fatigue, moral, ...)
+  Unite *m_unite; // Pointeur vers l'unité qu'il est
+  Case *m_case; // Pointeur vers la case où il se trouve
 
   
 public:
-  Agent(int _iden, Case* _c, Unite* _u);
+  Agent(int _iden, Case* _c, Unite* _u); // Constructeur
+  
+  // Getteurs
+  
   int getId() const;
   float getMultiplicateurV() const;
   const Case* getCase() const;
   const Unite* getUnite() const;
+  
+  // Setteurs
  
   void setCase(Case* _c);
   void setUnite(Unite* _u);  

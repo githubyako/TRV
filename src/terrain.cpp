f7d2e94f80ddea45d08b3fe5b1f16459817cfc94
tvoillemin <theo.voillemin@laposte.net>
2016-04-11 23:12:21 +0200

    Update terrain.cpp

--- src/terrain.cpp
+++ src/terrain.cpp
@@ -35,16 +35,16 @@ const std::vector< std::pair< Contrainte*, float > > Terrain::getContraintes() c
 }
 
 
-float Terrain::getContrainteDef(Contrainte* _nomContrainte) const
+float Terrain::getContrainteDef(Contrainte* _contrName) const
 {
   // On regarde si la contrainte _nomContrainte appartient au vector m_contraintes_defaut du terrain
   for(unsigned int i=0;i<m_contraintes_defaut.size();++i){
-    if((m_contraintes_defaut.at(i).first)->getNom()==_nomContrainte->getNom()){
+    if((m_contraintes_defaut.at(i).first)->getNom()==_contrName->getNom()){
       return m_contraintes_defaut.at(i).second;
     }
   }
   // Sinon, ce n'est pas normal, on lève donc une exception
-  throw new str_exception("Contrainte '" + _nomContrainte->getNom() + "' non trouvée dans les valeurs par défaut pour le terrain " + m_type);
+  throw new str_exception("Contrainte '" + _contrName->getNom() + "' non trouvée dans les valeurs par défaut pour le terrain " + m_type);
   return 0.0;
 }
 

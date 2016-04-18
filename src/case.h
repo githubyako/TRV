41c4be61c95ca9910c6cccc6718a4cca38fc1f8a
tvoillemin <theo.voillemin@laposte.net>
2016-04-18 11:06:55 +0200

    Update case.h

--- src/case.h
+++ src/case.h
@@ -12,6 +12,7 @@ class Case{
 private:
   unsigned int m_x,m_y; // Coordonnée x et y
   std::vector<std::pair<Contrainte*,float> >m_contraintes; // Contrainte (ex: ressource : avoine, blé...)
+  std::vector<Case*> m_cases_vois;
   bool m_obstacle; // Booléen si la case est, ou non, franchissable
   unsigned int m_somm; // Numéro du sommet associé à cette case dans le graphe
   Terrain * m_terrain; // Pointeur vers le terrain qu'est la case

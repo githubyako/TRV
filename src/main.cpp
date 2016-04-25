bf1fd6c4aa562812a0a09552bcf4b402fa86c242
tvoillemin <theo.voillemin@laposte.net>
2016-04-25 13:54:58 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@ int main(int argc, char **argv) {
     std::cout << "TRV" << std::endl;
     Controller* controller = Controller::create();
     try{
-      controller->initiateRules("regle.xml");
-      controller->initiateMap("map.txt");
+      controller->initiateRules("../regle.xml");
+      controller->initiateMap("../map.txt");
       controller->creer_agent(3,0,"Cavalier",0);
       controller->creer_agent(3,0,"Cavalier",1);
       controller->creer_agent(4,0,"Cavalier",0);

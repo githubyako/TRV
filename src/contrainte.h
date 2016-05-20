980590cf44b7b224f39f1ac5c2cbd302fc04296c
tvoillemin <theo.voillemin@laposte.net>
2016-05-20 15:23:14 +0200

    Update contrainte.h

--- src/contrainte.h
+++ src/contrainte.h
@@ -1,25 +1,76 @@
+/*
+ * <TRV pathfinding solution using genetic algorithm>
+ * Copyright (C) 2016  <copyright Roux Nicolas, Voillemin Théo, Talarmain Corentin>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/*!
+ *\file contrainte.h
+ *\author Nicolas Roux, Théo Voillemin, Corentin Talarmain
+ *\brief  Contrainte's file
+ * 
+ */
+
 #ifndef CONTRAINTE_H
 #define CONTRAINTE_H
 #include <string>
 
-// Classe représentant une contrainte (ex : ressource : avoine, blén...)
+/*! \class Contrainte
+ * \brief Class representing a Contrainte
+ * This class handle the Contrainte
+ */
 
 class Contrainte{
 private:
-  std::string m_nom; // Nom de la contrainte
+  std::string m_nom; /*!< Name of the Contrainte */
   
   // Constructeurs : private pour rendre inaccessible
+  
+  /*!
+   * \brief Default constructor
+   * 
+   */
   Contrainte();
+  
+  /*!
+   * \brief Copy constructor
+   * 
+   */
   Contrainte(Contrainte const & _contrainte); 
 public:
   
-  // Constructeur
+  // Constructor
+  /*!
+   * \brief Constructor.
+   * 
+   * \param _nom : Name of the Contrainte.
+   */
   Contrainte(std::string const & _nom); // seul constructeur qui servira
   
-  // Destructeur
+  // Destructor
+  
+  /*!
+   * \brief Destructor
+   */
   ~Contrainte();
   
-  // Getteur
+  // Getter
+  /*!
+   * \brief Getter of the Contrainte's name attribute.
+   */
   std::string const & getNom() const;
   
 };
